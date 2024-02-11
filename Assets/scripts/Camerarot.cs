b4f4f3b7a2f7ffcd00b3a0a44d06bab11a802366
SposobWQ <nikolajkaplanan74@gmail.com>
2024-02-11 22:12:18 +0800

    !@#

---
+++ Assets/scripts/Camerarot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camerarot : MonoBehaviour
+{
+    [SerializeField] private float maxangle;
+    [SerializeField] private float minangle;
+    [SerializeField] private Transform CamRotAxisTrans;
+    [SerializeField] private float RotSpeed;
+    void Update()
+    {
+        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y+Time.deltaTime*RotSpeed*Input.GetAxis("Mouse X"), 0);
+        var newAxisX = CamRotAxisTrans.localEulerAngles.x - Time.deltaTime * RotSpeed * Input.GetAxis("Mouse Y");
+        if (newAxisX>180) 
+        {
+            newAxisX -= 360;
+        }
+        newAxisX = Mathf.Clamp(newAxisX, minangle, maxangle);
+
+        CamRotAxisTrans.localEulerAngles = new Vector3(newAxisX, 0, 0);
+    }
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    private Vector3 _moveVector;
+    public float _speed;
+    public float _jumpforce;
+    private CharacterController _chcontr;
+    private float gravity = 9.8f;
+    [SerializeField] private float _fallVelocity;
+    void Start()
+    {
+        _chcontr = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)&& _chcontr.isGrounded)
+        {
+            _fallVelocity = -_jumpforce;
+        }
+        _moveVector = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            _moveVector += transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _moveVector -= transform.forward;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            _moveVector -= transform.right;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _moveVector += transform.right;
+        }
+        
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _chcontr.Move(_moveVector * _speed * Time.fixedDeltaTime);
+        _chcontr.Move(Vector3.down * _fallVelocity * Time.deltaTime);
+        _fallVelocity += gravity * Time.fixedDeltaTime;
+        if (_chcontr.isGrounded)
+        {
+                _fallVelocity = 0;
+        }
+
+
+
+    }
+}
